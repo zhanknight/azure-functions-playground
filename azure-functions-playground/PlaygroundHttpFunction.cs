7b21d97da5a43a4d4b0b2c6fed3b4d2908131075
zhanknight <20487008+zhanknight@users.noreply.github.com>
2024-09-05 15:19:34 -0400

    Now accepting posts

--- azure-functions-playground/PlaygroundHttpFunction.cs
+++ azure-functions-playground/PlaygroundHttpFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
 namespace azure_functions_playground
 {
@@ -15,10 +16,44 @@ namespace azure_functions_playground
         }
 
         [Function("PlaygroundHttpFunction")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")]
+        HttpRequest req, [FromBody] TemperatureData data)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            if (!Validator(data))
+            {
+                return new BadRequestObjectResult("Invalid data");
+            }
+            else
+            {
+                return new OkObjectResult("Data validated and accepted for processing!");
+            }
+        }
+
+        private bool Validator(TemperatureData data)
+        {
+           bool isValid = true;
+
+            if (data.DeviceId < 0 | data.DeviceId > 9)
+            {
+                isValid = false;
+            }
+            if (data.Time > DateTime.Now)
+            {
+                isValid = false;
+            }
+            if (data.Temperature < 0.0 | data.Temperature > .90)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        public class TemperatureData
+        {
+            public int DeviceId { get; set; }
+            public DateTime Time { get; set; }
+            public double Temperature { get; set; }
         }
     }
 }
